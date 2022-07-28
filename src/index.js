7e44d7e036bf5e1b06e187f70c661c5fdd2c970d
Leilany0206 <alondralei.delgado@gmail.com>
2022-07-28 11:38:54 -0500

    feat: agrega configuración CORS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const colors = require('colors');
+
+// CORS
+const cors = require('cors');
+
 const routerApi = require('./routes/main.controller')
 
 const app = express();
@@ -7,6 +11,24 @@ const port = 3000;
 
 // MIDDLEWARE
 app.use(express.json());
+
+// CORS
+const whitelist = ['http://127.0.0.1:5500']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+app.use(cors(corsOptions));
+
+// ROOT ENDPOINT
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API FundBecasMX')
+});
 
 app.listen(port, () => {
     console.log('Servidor express listening...'.rainbow)
